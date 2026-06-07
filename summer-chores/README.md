98a3417e2f45674c975cfb3f08d343700ff1fc8a
german-reghan <germanreghan8@gmail.com>
2026-06-07 16:52:03 -0400

    Added async\await version

--- summer-chores/README.md
+++ summer-chores/README.md
@@ -1,3 +1,6 @@
-Summer-chores is a  JavaScript project that:
--Uses callback functions to test if someone finishes their chores or falls asleep somewhere along the sequence. To run, type node callbackVersion.js in the terminal.
--Uses Promises to test if someone finishes their chores or falls asleep somewhere along the sequence. To run, type node promiseVersion.js in the terminal.
+Summer-chores is a  JavaScript project that has three main parts:
+The first part, callbackVersion.js uses callback functions to test if someone finishes their chores or falls asleep somewhere along the sequence. To run, type node callbackVersion.js in the terminal.
+
+The second part, promiseVersion.js uses Promises to test if someone finishes their chores or falls asleep somewhere along the sequence. To run, type node promiseVersion.js in the terminal.
+
+The third part, asyncAwaitVersion.js uses async/await to test if someone finishes their chores or falls asleep somewhere along the sequence. To run, type node asyncAwaitVersion.js in the terminal.
---
+++ summer-chores/asyncAwaitVersion.js
@@ -0,0 +1,84 @@
+function fellAsleep() {
+    return Math.random() < 0.5;
+}
+
+function mowYard(name) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`${name} mowed the yard.`);
+            resolve();
+        }, 2000);
+    });
+}
+
+function weedEat(name) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            
+            if (!fellAsleep()) {
+                console.log(`${name} finished using the weed eater.`);
+                resolve();
+            } else {
+                reject(`${name} fell asleep after using the weed eater.`);
+            }
+        }, 1500);
+    });
+}
+
+function trimHedges(name) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            
+            if (!fellAsleep()) {
+                console.log(`${name} finished trimming the hedges.`);
+                resolve();
+            } else {
+                reject(`${name} fell asleep after trimming the hedges.`);
+            }
+        }, 1000);
+    });
+}
+
+function collectWood(name) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            
+            if (!fellAsleep()) {
+                console.log(`${name} finished collecting the wood.`);
+                resolve();
+            } else {
+                reject(`${name} fell asleep after collecting the wood.`);
+            }
+        }, 2500);
+    });
+}
+
+function waterGarden(name) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            
+            if (!fellAsleep()) {
+                console.log(`${name} finished watering the garden.`);
+                resolve();
+            } else {
+                reject(`${name} fell asleep after watering the garden.`);
+            }
+        }, 500);
+    });
+}
+
+async function doSummerChores(name) {
+    try {
+        await mowYard(name);
+        await weedEat(name);
+        await trimHedges(name);
+        await collectWood(name);
+        await waterGarden(name);
+        
+        console.log(`${name} finished all their chores!`);
+        } catch(error) {
+            console.log(error);
+        }
+}
+
+doSummerChores("Reghan");
